438476a9568e9575175f44bd2a22ad1d1c736162
Kolian0v <111593864+Kolian0v@users.noreply.github.com>
2023-02-23 20:26:14 +0300

    last_commit

---
+++ ft_atoi.c
@@ -0,0 +1,43 @@
+#include "philo.h"
+
+static int	ft_atoi2(const char *str, int *dig)
+{
+	int	i;
+
+	i = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	if (str[i] == '-')
+	{
+		*dig = -1;
+		i++;
+	}
+	return (i);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	k;
+	int	res;
+	int	dig;
+
+	dig = 1;
+	i = 0;
+	res = 0;
+	i = ft_atoi2(str, &dig);
+	if (i > 0)
+		return (-1);
+	k = 0;
+	while (str[i + k] != '\0')
+	{
+		if (k > 9)
+			return (0);
+		if (*(str + i + k) >= '0' && *(str + i + k) <= '9')
+			res = (res * 10 + *(str + i + k) - 48);
+		else
+			return (0);
+		k++;
+	}
+	return (dig * res);
+}
